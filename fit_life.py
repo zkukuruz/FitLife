3507a604700ecf140d45bf6d1be95dee616549d8
Zaur Kuzmin <zaur.kuzmin1993@gmail.com>
2026-09-10 13:21:52 +0300

    Исправлены замечания ревьюера

--- fit_life.py
+++ fit_life.py
@@ -1,19 +1,29 @@
-user_name = input("Как тебя зовут? ")
-user_age = int(input("Сколько тебе лет? "))
+from constants import WATER_PER_KG, ML_IN_LITER
 
+user_name = input("Как тебя зовут? ").title()
 
-user_weight = float(input("Укажи свой вес в кг: "))
-user_height = float(input("Укажи свой рост в метрах (например, 1.75): "))
+user_age = int(input("Сколько тебе полных лет? Введите число: "))
 
+user_weight = float(
+    input("Укажи свой вес в кг: ").replace(",", ".")
+)
+
+height_input = input(
+    "Укажи свой рост в метрах (например, 1.75): "
+)
+
+height_input = height_input.replace(",", ".")
+user_height = float(height_input)
 
 bmi = user_weight / (user_height ** 2)
 
-
-water_needed = user_weight * 30
-
+water_needed = user_weight * WATER_PER_KG
 
 print(f"Привет, {user_name}!")
 print(f"Тебе {user_age} лет.")
 print(f"Твой Индекс Массы Тела: {round(bmi, 1)}")
-print(f"Рекомендуемая норма воды: {round(water_needed / 1000, 2)} л. в день")
+print(
+    f"Рекомендуемая норма воды: "
+    f"{round(water_needed / ML_IN_LITER, 2)} л. в день"
+)
 print("Расчет окончен. Будьте здоровы!")
